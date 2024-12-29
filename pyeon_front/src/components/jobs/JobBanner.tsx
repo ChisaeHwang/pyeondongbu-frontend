557e0a8a436e6a9abf211caceb74dbaad87e62ec
ChisaeHwang <cwh73090@naver.com>
2024-12-30 01:23:49 +0900

    배너 작업 및 모바일 폼 작업

--- pyeon_front/src/components/jobs/JobBanner.tsx
+++ pyeon_front/src/components/jobs/JobBanner.tsx
@@ -3,11 +3,16 @@ import React from "react";
 const JobBanner = () => {
   return (
     <div className="mb-6 mt-6">
-      <div className="w-full h-[320px] rounded-lg overflow-hidden">
+      <div className="w-full h-[200px] sm:h-[160px] md:h-[200px] rounded-lg overflow-hidden">
+        <img
+          src="https://ifh.cc/g/Dvzz2b.jpg"
+          alt="배너 이미지"
+          className="w-full h-full object-cover block sm:hidden"
+        />
         <img
           src="https://ifh.cc/g/mr3J95.jpg"
           alt="배너 이미지"
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover hidden sm:block"
         />
       </div>
     </div>
