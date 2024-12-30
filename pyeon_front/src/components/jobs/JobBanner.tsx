7c0f060953d576a8dcaec3b296f0ed52d787e965
ChisaeHwang <cwh73090@naver.com>
2024-12-30 19:42:32 +0900

    데이터 수정 및 배너 수정 및 구인글 상세 내용 HTML으로 수정

--- pyeon_front/src/components/jobs/JobBanner.tsx
+++ pyeon_front/src/components/jobs/JobBanner.tsx
@@ -4,13 +4,15 @@ const JobBanner = () => {
   return (
     <div className="mb-6 mt-6">
       <div className="w-full h-[200px] sm:h-[160px] md:h-[200px] rounded-lg overflow-hidden">
+        {/* 모바일 버전 배너 이미지 */}
         <img
-          src="https://ifh.cc/g/Dvzz2b.jpg"
+          src="https://ifh.cc/g/r9v1dP.jpg"
           alt="배너 이미지"
           className="w-full h-full object-cover block sm:hidden"
         />
+        {/* 데스크톱 버전 배너 이미지 */}
         <img
-          src="https://ifh.cc/g/mr3J95.jpg"
+          src="https://ifh.cc/g/a3SCgJ.jpg"
           alt="배너 이미지"
           className="w-full h-full object-cover hidden sm:block"
         />
