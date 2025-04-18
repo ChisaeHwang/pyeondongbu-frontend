8224a55a5e01a29c2bcf04c7823bb8fe45e3dfe6
ChisaeHwang <cwh73090@naver.com>
2025-04-18 10:44:07 +0900

    fix: 레이아웃 문제 해결

---
+++ pyeon_front/src/components/common/AdSidebar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import AdBanner from "../ads/AdBanner";
+
+interface AdSidebarProps {
+  className?: string;
+}
+
+const AdSidebar: React.FC<AdSidebarProps> = ({ className = "" }) => {
+  return (
+    <div className={`ad-sidebar ${className}`}>
+      <AdBanner
+        adClient="ca-pub-9895707756303015"
+        adSlot="5409996939"
+        width={300}
+        height={600}
+        format="vertical"
+        responsive={false}
+        className="mb-4"
+      />
+    </div>
+  );
+};
+
+export default AdSidebar;
