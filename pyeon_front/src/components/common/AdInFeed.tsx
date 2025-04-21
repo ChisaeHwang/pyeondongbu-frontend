aad9a4523dba4e4ad7c00d3f08d1cedf3dd174ab
ChisaeHwang <cwh73090@naver.com>
2025-04-21 16:06:27 +0900

    fix: 광고 hegiht 수정

--- pyeon_front/src/components/common/AdInFeed.tsx
+++ pyeon_front/src/components/common/AdInFeed.tsx
@@ -85,15 +85,21 @@ const AdInFeed: React.FC<AdInFeedProps> = ({
   const adSlot = isMobile ? "7123368001" : "4688776358";
   const layoutKey = isMobile ? "-hk-1-x-dr+wr" : "-fu-3i+7j-dp+53";
 
+  // 모바일과 데스크탑에 따른 높이 설정
+  const minHeight = isMobile ? "250px" : "280px";
+
   return (
     <div
       id="ad-container"
       className={`bg-[#25262b] rounded-lg border border-[#2c2d32] overflow-hidden hover:border-[#3a3b40] transition-colors ${className}`}
-      style={style}
+      style={{ ...style, minHeight }}
     >
-      <div className="p-4">
+      <div className="p-4 h-full">
         {/* 광고 컨텐츠 */}
-        <div className="w-full min-h-[90px] md:min-h-[100px] relative">
+        <div
+          className="w-full h-full relative"
+          style={{ minHeight: isMobile ? "200px" : "230px" }}
+        >
           {!isLoaded && isVisible && (
             <div className="absolute inset-0 flex items-center justify-center">
               <div className="animate-pulse bg-[#2c2d32] w-full h-full rounded opacity-50"></div>
@@ -105,7 +111,9 @@ const AdInFeed: React.FC<AdInFeedProps> = ({
               style={{
                 display: "block",
                 textAlign: "center",
-                minHeight: "90px",
+                width: "100%",
+                height: "100%",
+                minHeight: isMobile ? "200px" : "230px",
               }}
               data-ad-format="fluid"
               data-ad-layout-key={layoutKey}
